7b0dce146606a8c92844036bd720f97b6a857b18
Dhanashree <thoredhanashree@gmail.com>
2023-07-17 14:41:04 +0530

    Added first project-

---
+++ src/demoPackage/ConditionalStatements.java
@@ -0,0 +1,16 @@
+package demoPackage;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args)
+	{
+		int Cash = 100000;
+		if (Cash>=100000) // condition (true)
+		{
+			//body
+			System.out.println("You can take the bike");
+		}
+		System.out.println("You can quit the showroom");
+	}
+
+}
